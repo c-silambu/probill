79f8203af4935bceac50bfd3579cf9c274d0a272
c-silambu <144410442+c-silambu@users.noreply.github.com>
2026-01-14 22:57:57 +0530

    Fix AdminLayout CSS import and build error

--- billing-frontend/src/services/axios.js
+++ billing-frontend/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API=axios.create({
-    baseURL:process.env.REACT_APP_API_URL,
+    baseURL:"https://localhost:3000",
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token");
